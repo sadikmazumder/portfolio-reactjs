395bd3f714c2580b8ca06d6ffa9e8e4a6a8acf41
sadikmazumder <sadikmazumder@gmail.com>
2023-06-10 02:27:21 +0530

    portfolio component design done

--- src/containers/portfolio/index.jsx
+++ src/containers/portfolio/index.jsx
@@ -1,7 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
+import PageHeaderContent from "../../components/pageHeaderContent";
+import { AiFillProject } from "react-icons/ai";
+import { filterOptions, portFolioData } from "./utils";
+import "./style.scss";
 
 const Portfolio = () => {
-  return <div>Portfolio</div>;
+  const [filterValue, setFilterValue] = useState(1);
+  const [hoveredIndex, setHoveredIndex] = useState(null);
+
+  const handleFilter = (id) => {
+    setFilterValue(id);
+  };
+
+  console.log(filterValue);
+
+  const filteredportfolioData =
+    filterValue === 1
+      ? portFolioData
+      : portFolioData.filter((item) => item.sectionId === filterValue);
+
+  return (
+    <section id="portfolio" className="portfolio">
+      <PageHeaderContent
+        headerText="My Portfolio"
+        icon={<AiFillProject size={40} />}
+      />
+
+      <div className="portfolio__content">
+        <ul className="portfolio__content__filter">
+          {filterOptions.map((option, key) => (
+            <li
+              onClick={() => handleFilter(option.id)}
+              key={`filter${option.id}`}
+              className={option.id === filterValue ? "active" : ""}
+            >
+              {option.label}
+            </li>
+          ))}
+        </ul>
+
+        <div className="portfolio__content__cards">
+          {filteredportfolioData.map((item, key) => (
+            <div
+              onMouseEnter={() => setHoveredIndex(key)}
+              onMouseLeave={() => setHoveredIndex(null)}
+              key={key}
+              className="portfolio__content__cards__item"
+            >
+              <div className="portfolio__content__cards__item__img-wrapper">
+                <a>
+                  <img src={item.image} alt="project image" />
+                </a>
+              </div>
+              <div className="overlay">
+                {hoveredIndex === key && (
+                  <div>
+                    <p>{item.projectName}</p>
+                    <button>Visit</button>
+                  </div>
+                )}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Portfolio;
---
+++ src/containers/portfolio/utils.js
@@ -0,0 +1,53 @@
+import ImageOne from "../../images/image1.jpg";
+import ImageTwo from "../../images/image2.jpg";
+import ImageThree from "../../images/image3.jpg";
+import ImageFour from "../../images/image4.jpg";
+import ImageFive from "../../images/image5.jpg";
+
+export const portFolioData = [
+  {
+    sectionId: 2,
+    projectName: "ToDo Application",
+    projectLink: "", // project link here based on your choice
+    image: ImageOne,
+  },
+  {
+    sectionId: 2,
+    projectName: "Shoping Cart application ",
+    projectLink: "", // project link here based on your choice
+    image: ImageTwo,
+  },
+  {
+    sectionId: 2,
+    projectName: "Calculator App",
+    projectLink: "", // project link here based on your choice
+    image: ImageThree,
+  },
+  {
+    sectionId: 3,
+    projectName: "Design App",
+    projectLink: "", // project link here based on your choice
+    image: ImageFour,
+  },
+  {
+    sectionId: 3,
+    projectName: "Design App",
+    projectLink: "", // project link here based on your choice
+    image: ImageFive,
+  },
+];
+
+export const filterOptions = [
+  {
+    label: "All",
+    id: 1,
+  },
+  {
+    label: "Development",
+    id: 2,
+  },
+  {
+    label: "Design",
+    id: 3,
+  },
+];
